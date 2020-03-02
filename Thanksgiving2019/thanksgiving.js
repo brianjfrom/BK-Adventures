940a29b3c9056cdf5ef397043f63534d4bc9bf92
brianjfrom <47318321+brianjfrom@users.noreply.github.com>
2020-03-01 22:59:21 -0700

    Create thanksgiving.js

---
+++ Thanksgiving2019/thanksgiving.js
@@ -0,0 +1,33 @@
+function myFunction () {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+function myFunction1(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
+
+// scrollbar
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myScrollBar").style.width = scrolled + "%"
+}
